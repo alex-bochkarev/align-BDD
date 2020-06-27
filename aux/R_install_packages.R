0e0302176e25949ff73fdcc368c4f2f7c76c5ccd
Alexey Bochkraev <a@bochkarev.io>
2020-06-27 10:49:22 +0200

    corresponds to the draft v0.8.5

--- aux/R_install_packages.R
+++ aux/R_install_packages.R
@@ -1,4 +1,4 @@
 install.packages(c(
-    "ggplot2","gridExtra","latex2exp","stringr","dplyr","tidyr","optparse", "ggridges"),
+    "ggplot2","gridExtra","latex2exp","stringr","dplyr","tidyr","optparse", "ggridges", "xtable"),
     repos="http://cran.us.r-project.org"
     )
