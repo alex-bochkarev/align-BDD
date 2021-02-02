f2e15b2b5f9984d8d3a672e483820dfb61319aa1
Alexey Bochkarev <a@bochkarev.io>
2021-02-02 23:36:43 +0100

    availability and covering BDD generation (simple tests passing)

--- UFL.py
+++ UFL.py
@@ -2,6 +2,9 @@
 UFL -- Uncapacitated Facility Location
 (testing align-BDD machinery for specific applications)
 
+General notes:
+--------------
+- on indices: `j` is used for consumers, `i` -- for facilities
 ---
 (c) A. Bochkarev, Clemson University, 2021
 abochka@clemson.edu
@@ -11,35 +14,40 @@ import BDD as DD
 
 import numpy as np
 
-m = 4; n = 2
 
-S = []
+def draw_problem_dia(S, n, filename="run_logs/problem_dia.gv"):
+    """Draws the bipartite graph describing the probelm"""
 
-## define a toy problem
-S.append([1]); S.append([1,2]); S.append([1,2]); S.append([2])
-F = [1, 1]
-λ = 0.5
+    dot = Digraph('G', comment="Uncapacitated Facility Location")
+    for i in range(n):
+        dot.node(f"F{i+1}", shape='doublecircle', style='filled', color='lightgrey')
 
-## draw a problem diagram
-dot = Digraph('G',comment="Uncapacitated Facility Location")
-for i in range(n):
-    dot.node(f"F{i+1}", shape='doublecircle', style='filled', color='lightgrey')
+    for j, S_j in enumerate(S):
+        dot.node(f"C{j+1}", shape='circle')
+        for i in S_j:
+            dot.edge(f"F{i}", f"C{j+1}")
 
-for j, S_j in enumerate(S):
-    dot.node(f"C{j}", shape='circle')
-    for i in S_j:
-        dot.edge(f"F{i}", f"C{j}")
+    dot.view(filename=filename)
 
-dot.view()
 
-print([f"z{i}-{j+1}" for j in range(m) for i in S[j]])
 
-print(len(np.sum(S)))
+def create_covering_BDD(S):
+    """Create and return the diagram given the instance.
 
-def create_covering_BDD(S,n,m):
-    ## 1) create covering BDD
+    Notes:
+        - encodes the fact that all customers need to be covered
+          by at least one facility.
+        - assumes no edge costs, does *not* allow
+        for arbitrary g(·) function.
+
+    Parameters:
+      S: list -- of adjacency lists. len(S) = no. of consumers
+    """
+
+    m = len(S)
+
     C = DD.BDD(
-        N=len(np.sum(S)),
+        N=len(np.sum(S)), # number of z-variables
         vars=[f"z{i}-{j+1}" for j in range(m) for i in S[j]]
     )
 
@@ -48,9 +56,10 @@ def create_covering_BDD(S,n,m):
     # current nodes
     s_maybe = root
     s_no = None
-    # TODO: assert all Sj != []
 
     for j in range(m):
+        assert len(S[j]) > 0
+
         s_yes = None
         if j == m-1:
             S_j = S[j][:-1]
@@ -58,15 +67,14 @@ def create_covering_BDD(S,n,m):
             S_j = S[j]
 
         for idx, i in enumerate(S_j):
-            print(f"j={j},idx={idx}, maybenode={s_maybe.id}")
             new_maybe = C.addnode(s_maybe, "lo")
             new_yes = C.addnode(s_maybe, "hi")
 
-            if not (s_yes is None):
+            if s_yes is not None:
                 s_yes.link(new_yes, "hi")
                 s_yes.link(new_yes, "lo")
 
-            if not (s_no is None):
+            if s_no is not None:
                 if idx < len(S[j])-1:
                     new_no = C.addnode(s_no)
                 else:
@@ -85,7 +93,6 @@ def create_covering_BDD(S,n,m):
                 s_maybe = new_yes
                 s_yes.link(new_yes, "hi")
                 s_yes.link(new_yes, "lo")
-                print(f"LAST F: yes:{s_yes.id}, no:{s_no.id}, maybe:{s_maybe.id}")
             else:
                 s_maybe = new_maybe
 
@@ -96,11 +103,117 @@ def create_covering_BDD(S,n,m):
         s_yes.link(C.nodes[DD.NTRUE], "hi")
         s_yes.link(C.nodes[DD.NTRUE], "lo")
 
-    if not (s_no is None):
+    if not s_no is None:
         s_no.link(C.nodes[DD.NFALSE], "hi")
         s_no.link(C.nodes[DD.NFALSE], "lo")
 
     return C
 
-create_covering_BDD(S, n, m).dump_gv().view()
-## 2) create availability BDD
+
+def build_Sf(S):
+    """returns the list of facility neighborhoods
+
+    Arguments
+    ---------
+    S: list -- of consumer neighborhoods (facility IDs)
+    """
+
+    Sf = dict()
+    for j in range(len(S)):
+        for facility in S[j]:
+            if facility in Sf.keys():
+                Sf[facility].add(j+1)
+            else:
+                Sf.update({facility : set([j+1])})
+
+    return Sf
+
+
+
+def create_availability_BDD(S):
+    """Encodes the fact that "turn-on" (x) and "cover"(z) decisions
+    need to be consistent.
+    """
+    m = len(S)
+
+    Sf = build_Sf(S)
+    n = len(Sf.keys())
+
+    var_names = sum([[f"x{facility}"] + [f"z{facility}-{j}"
+                                            for j in Sf[facility]]
+                        for facility in Sf],[])
+
+    A = DD.BDD(N=len(var_names), vars=var_names)
+
+    s_main = A.addnode(None);
+
+    s_false = None
+    for i in range(1,n+1):
+        # create a block per facility
+        s_yes = A.addnode(s_main, "hi")
+        s_no = A.addnode(s_main, "lo")
+
+        if s_false is not None:
+            new_f = A.addnode(s_false, "hi")
+            s_false.link(new_f, "lo")
+            s_false = new_f
+
+        consumers_i = list(Sf[i])
+        for j in consumers_i[:-1]:
+            new_yes = A.addnode(s_yes, "hi")
+            new_no = A.addnode(s_no, "lo")
+
+            new_f = A.addnode(s_yes, "lo")
+            s_no.link(new_f, "hi")
+
+            if s_false is not None:
+                s_false.link(new_f, "hi")
+                s_false.link(new_f, "lo")
+
+            s_yes = new_yes
+            s_no = new_no
+            s_false = new_f
+
+        if i < n:
+            s_main = A.addnode(s_yes, "hi")
+            s_no.link(s_main, "lo")
+
+            new_f = A.addnode(s_false, "hi")
+            s_false.link(new_f, "lo")
+            s_yes.link(new_f, "lo")
+            s_no.link(new_f, "hi")
+            s_false = new_f
+        else:
+            # this is a special case:
+            # we'd need to link to the terminal nodes
+            s_yes.link(A.nodes[DD.NTRUE], "hi");
+            s_yes.link(A.nodes[DD.NFALSE], "lo")
+            s_no.link(A.nodes[DD.NTRUE], "lo")
+            s_no.link(A.nodes[DD.NFALSE], "hi")
+            s_false.link(A.nodes[DD.NFALSE],"hi")
+            s_false.link(A.nodes[DD.NFALSE],"lo")
+
+    return A
+
+# testing
+def test_DD_creation(S, name):
+    """Testing unit: BDD generation"""
+
+    # unpack the problem from S
+    draw_problem_dia(S, n, filename="run_logs/"+name+"_problem.gv")
+
+    create_covering_BDD(S).dump_gv().view(filename="run_logs/"+name+"_covering.gv")
+    create_availability_BDD(S).dump_gv().view(filename="run_logs/"+name+"_avail.gv")
+
+## define a toy problem
+S = [[1], [1,2], [1,2], [2]]
+test_DD_creation(S, "toy")
+
+## define a slightly more complicated one
+S = [[1,4], [1,4],[1,4],
+        [1,2], [1,2],
+        [2,3],
+        [2,3,5],
+        [3,5]]
+
+test_DD_creation(S, "simple")
