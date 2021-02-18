3faebd33196b8ddd5be74488636e7589e1c2c8b7
Alexey Bochkarev <a@bochkarev.io>
2021-02-18 13:52:37 +0100

    removed x_i

--- UFL.py
+++ UFL.py
@@ -5,6 +5,7 @@ UFL -- Uncapacitated Facility Location
 General notes:
 --------------
 - on indices: `j` is used for consumers, `i` -- for facilities
+
 ---
 (c) A. Bochkarev, Clemson University, 2021
 abochka@clemson.edu
@@ -17,7 +18,7 @@ from gurobipy import GRB
 import BDD as DD
 
 
-def draw_problem_dia(S, f, c, g, filename="run_logs/problem_dia.gv"):
+def draw_problem_dia(S, f, g, filename="run_logs/problem_dia.gv"):
     """Draws the bipartite graph describing the problem"""
 
     n = len(build_Sf(S).keys())
@@ -30,22 +31,21 @@ def draw_problem_dia(S, f, c, g, filename="run_logs/problem_dia.gv"):
 
     for j in range(len(S)):
         dia.node(f"C{j+1}", shape='circle',
-                 label=f"C{j+1}\ng=({','.join([str(g[(k, l)]) for (k, l) in g.keys() if k==i])})")
+                 label=f"C{j+1}\ng=({','.join([str(g[(k, l)]) for (k, l) in g.keys() if k==(j+1)])})")  # noqa: E501
 
     for j, S_j in enumerate(S):
         for i in S_j:
-            dia.edge(f"F{i}", f"C{j+1}", label=f"{c[(i, j+1)]}")
+            dia.edge(f"F{i}", f"C{j+1}")
 
     dia.view(filename=filename)
 
 
-def create_covering_BDD_wg(S, c, g):  # pylint: disable=all
-    """Creates and returns the BDD given the instance.
+def create_covering_BDD_wg(S, g):  # pylint: disable=all
+    """Creates and returns "covering" BDD given an UFL instance.
 
     Args:
         S (list): of adjacency lists
-        c (dict): costs of covering (facility_id, consumer_id)
-        g (dict): overlapping costs (g(k,n) is a cost of `n`
+        g (dict): overlapping costs, g(k,n) is a cost of `n`
             overlaps for consumer `k`)
 
     Returns:
@@ -55,66 +55,39 @@ def create_covering_BDD_wg(S, c, g):  # pylint: disable=all
     m = len(S)
 
     C = DD.BDD(
-        N=len(np.sum(S)), # number of z-variables
+        N=len(np.sum(S)),  # number of z-variables
         vars=[f"z{i}-{j+1}" for j in range(m) for i in S[j]],
         weighted=True)
 
     root = C.addnode(None)
     current_layer = [root]
-    s_false = None
 
     for j in range(1, m+1):
         assert len(S[j-1]) > 0
 
-        for i in range(1, len(S[j-1])):
-            next_layer = [C.addnode(n, "lo") for n in current_layer]
-            if s_false is not None:
-                new_f = C.addnode(s_false, "hi")
-                C.link(s_false.id, new_f.id, "lo")
-                s_false = new_f
+        for i in range(1, len(S[j-1])):  # all *but* the last nearby facility
+            next_layer = [C.addnode(n, "lo") for n in current_layer] + \
+                [C.addnode(current_layer[-1], "hi")]
 
             for q in range(len(current_layer)-1):
-                print(f"q={q},j={j},i={i}")
-                C.link(current_layer[q].id, next_layer[q+1].id, "hi",
-                       edge_weight=c[(S[j-1][i-1], j)])
-            next_layer.append(C.addnode(current_layer[-1], "hi",
-                                        edge_weight=c[(S[j-1][i-1], j)]))
+                C.link(current_layer[q].id, next_layer[q+1].id, "hi")
+
             current_layer = next_layer
 
         # process the last layer of the subnetwork corresponding
-        # to consumer `j`: that is always a special case
-        # FIXME: a tiny optimization possible (redundant lines)
+        # to consumer `j`: this is always a special case
         if j < m:
-            if s_false is not None:
-                new_f = C.addnode(s_false, "hi")
-                C.link(s_false.id, new_f.id, "lo")
-                C.link(current_layer[0].id, new_f.id, "lo")
-            else:
-                new_f = C.addnode(current_layer[0], "lo")
-
-            target = C.addnode(current_layer[0], "hi",
-                               edge_weight=c[(S[j-1][-1], j)] + g[(j, 1)])
-            next_layer = [target]
+            target = C.addnode(current_layer[0], "lo")
         else:
-            new_f = C.nodes[DD.NFALSE]
-            if s_false is not None:
-                C.link(s_false.id, new_f.id, "lo")
-                C.link(s_false.id, new_f.id, "hi")
-                C.link(current_layer[0].id, new_f.id, "lo")
-            else:
-                new_f = C.addnode(current_layer[0], "lo")
+            target = C.T
 
-            target = C.nodes[DD.NTRUE]
-            C.link(current_layer[0].id, target.id, "hi",
-                   edge_weight=c[(S[j-1][-1], j)] + g[(j, 1)])
-
-        for idx, n in enumerate(current_layer[1:]):
+        for idx, n in enumerate(current_layer):
             C.link(n.id, target.id, "hi",
-                   edge_weight=c[(S[j-1][-1], j)] + g[(j, 2+idx)])
-            C.link(n.id, target.id, "lo")
+                   edge_weight=g[(j, idx+1)])
+            C.link(n.id, target.id, "lo",
+                   edge_weight=g[(j, idx)])
 
-        current_layer = next_layer
-        s_false = new_f
+        current_layer = [target]
 
     return C
 
@@ -136,7 +109,7 @@ def create_covering_BDD(S, c):
     m = len(S)
 
     C = DD.BDD(
-        N=len(np.sum(S)), # number of z-variables
+        N=len(np.sum(S)),  # number of z-variables
         vars=[f"z{i}-{j+1}" for j in range(m) for i in S[j]],
         weighted=True
     )
@@ -193,7 +166,7 @@ def create_covering_BDD(S, c):
         C.link(s_yes.id, DD.NTRUE, "hi", edge_weight=c[(i, j+1)])
         C.link(s_yes.id, DD.NTRUE, "lo")
 
-    if not s_no is None:
+    if s_no is not None:
         C.link(s_no.id, DD.NFALSE, "hi", edge_weight=c[(i, j+1)])
         C.link(s_no.id, DD.NFALSE, "lo")
 
@@ -214,10 +187,9 @@ def build_Sf(S):
             if facility in Sf.keys():
                 Sf[facility].add(j+1)
             else:
-                Sf.update({facility : set([j+1])})
+                Sf.update({facility: set([j+1])})
 
     return Sf
-
 
 
 def create_availability_BDD(S, f):
@@ -230,27 +202,26 @@ def create_availability_BDD(S, f):
     Sf = build_Sf(S)
     n = len(Sf.keys())
 
-    var_names = sum([[f"x{facility}"] + [f"z{facility}-{j}"
-                                            for j in Sf[facility]]
-                        for facility in Sf],[])
+    var_names = [f"z{facility}-{j}"
+                 for facility in Sf for j in Sf[facility]]
 
     A = DD.BDD(N=len(var_names), vars=var_names, weighted=True)
 
-    s_main = A.addnode(None);
+    s_main = A.addnode(None)
 
     s_false = None
-    for i in range(1,n+1):
+    for i in range(1, n+1):
         # create a block per facility
         s_yes = A.addnode(s_main, "hi", edge_weight=f[i])
         s_no = A.addnode(s_main, "lo")
 
         if s_false is not None:
-            new_f = A.addnode(s_false, "hi", edge_weight=f[i])
+            new_f = A.addnode(s_false, "hi")
             A.link(s_false.id, new_f.id, "lo")
             s_false = new_f
 
         consumers_i = list(Sf[i])
-        for j in consumers_i[:-1]:
+        for j in consumers_i[1:-1]:
             new_yes = A.addnode(s_yes, "hi")
             new_no = A.addnode(s_no, "lo")
 
@@ -258,8 +229,8 @@ def create_availability_BDD(S, f):
             A.link(s_no.id, new_f.id, "hi")
 
             if s_false is not None:
-               A.link(s_false.id, new_f.id, "hi")
-               A.link(s_false.id, new_f.id, "lo")
+                A.link(s_false.id, new_f.id, "hi")
+                A.link(s_false.id, new_f.id, "lo")
 
             s_yes = new_yes
             s_no = new_no
@@ -277,12 +248,12 @@ def create_availability_BDD(S, f):
         else:
             # this is a special case:
             # we'd need to link to the terminal nodes
-            A.link(s_yes.id,DD.NTRUE, "hi");
-            A.link(s_yes.id,DD.NFALSE, "lo")
-            A.link(s_no.id,DD.NTRUE, "lo")
-            A.link(s_no.id,DD.NFALSE, "hi")
-            A.link(s_false.id,DD.NFALSE,"hi")
-            A.link(s_false.id,DD.NFALSE,"lo")
+            A.link(s_yes.id, DD.NTRUE, "hi")
+            A.link(s_yes.id, DD.NFALSE, "lo")
+            A.link(s_no.id, DD.NTRUE, "lo")
+            A.link(s_no.id, DD.NFALSE, "hi")
+            A.link(s_false.id, DD.NFALSE, "hi")
+            A.link(s_false.id, DD.NFALSE, "lo")
 
     return A
 
@@ -294,8 +265,10 @@ def test_DD_creation(S, f, c, name):
     # unpack the problem from S
     draw_problem_dia(S, f, c, filename="run_logs/"+name+"_problem.gv")
 
-    create_covering_BDD(S, c).dump_gv().view(filename="run_logs/"+name+"_covering.gv")
-    create_availability_BDD(S, f).dump_gv().view(filename="run_logs/"+name+"_avail.gv")
+    create_covering_BDD(S, c).dump_gv().view(filename="run_logs/" +
+                                             name+"_covering.gv")
+    create_availability_BDD(S, f).dump_gv().view(filename="run_logs/" +
+                                                 name+"_avail.gv")
 
 
 def build_MIP(S, f, c, λ):
@@ -388,11 +361,12 @@ def add_BDD_to_MIP(D, model=None, x=None, prefix=""):
 
         inc_nodes_new = dict()
         for n in L:
-
+            inflow = gp.LinExpr(0.0)
             if n.id == DD.NROOT:
                 inflow = gp.LinExpr(1.0)
             else:
-                inflow = gp.quicksum(v_i for v_i in incoming_flows[n.id])
+                if n.id in incoming_flows.keys():
+                    inflow = gp.quicksum(v_i for v_i in incoming_flows[n.id])
 
             if n.id == DD.NTRUE:
                 outflow = gp.LinExpr(1.0)
@@ -430,9 +404,9 @@ def add_BDD_to_MIP(D, model=None, x=None, prefix=""):
             constraints.append(model.addConstr(
                 x[D.vars[l_no-1]] == yes_sum, name=f"{prefix}bin-link-{l_no}"))
 
-
     model.update()
     return model, constraints, v, x
+
 
 def test_BDD_to_MIP():
     """Tests making a MIP from a single BDD"""
@@ -450,11 +424,13 @@ def test_BDD_to_MIP():
     m, c, v, x = add_BDD_to_MIP(D=B, model=m, x=x, prefix="B_")
     m.display()
 
+
 def test_build_simple_MIP():
     """Tests a simple MIP building function"""
-    S = [[1], [1,2], [1,2], [2]]
-    f = { 1: 0.5, 2:0.7 }
-    c = {(1,1):1.1, (1,2):1.2, (2,2):2.2, (1,3):1.3, (2,3):2.3, (2,4):2.4 }
+    S = [[1], [1, 2], [1, 2], [2]]
+    f = {1: 0.5, 2: 0.7}
+    c = {(1, 1): 1.1, (1, 2): 1.2, (2, 2): 2.2, (1, 3): 1.3,
+         (2, 3): 2.3, (2, 4): 2.4}
     λ = 0.5
 
     test_DD_creation(S, f, c, "toy")
@@ -462,29 +438,31 @@ def test_build_simple_MIP():
     m = build_MIP(S, f, c, λ)
     m.display()
 
+
 def test_BDD_build():
     """Runs a simple test against a toy problem"""
 
-
-    S = [[1], [1,2], [1,2], [2]]
+    S = [[1], [1, 2], [1, 2], [2]]
     f = {1: 0.5, 2: 0.7}
-    c = {(1, 1): 0.11,  (1, 2): 0.12,  (2, 2): 0.22,  (1, 3): 0.13,  (2, 3): 0.23,  (2, 4): 0.24}
+    c = {(1, 1): 0.11,  (1, 2): 0.12,  (2, 2): 0.22,  (1, 3): 0.13,
+         (2, 3): 0.23,  (2, 4): 0.24}
     g = {
         (1, 0): 0,  (2, 0): 0,  (3, 0): 0,  (4, 0): 0,
         (1, 1): 1,  (2, 1): 1,  (3, 1): 1,  (4, 1): 1,
         (1, 2): 2,  (2, 2): 2,  (3, 2): 2,  (4, 2): 2}
 
     draw_problem_dia(S, f, c, g)
-    C = create_covering_BDD_wg(S, c, g)
+    C = create_covering_BDD_wg(S, g)
     C.show(x_prefix='', filename="covering.dot", dir="run_logs")
     A = create_availability_BDD(S, f)
     A.show(x_prefix='', filename="availability.dot", dir="run_logs")
 
-def test_BDD_to_MIP_wg(S, f, c, g):
+
+def test_BDD_to_MIP_wg(S, f, g):
     """Runs a simple test against a toy problem"""
 
-    draw_problem_dia(S, f, c, g)
-    C = create_covering_BDD_wg(S, c, g)
+    draw_problem_dia(S, f, g)
+    C = create_covering_BDD_wg(S, g)
     C.show(x_prefix='', filename="covering.dot", dir="run_logs")
     A = create_availability_BDD(S, f)
     A.show(x_prefix='', filename="availability.dot", dir="run_logs")
@@ -493,14 +471,15 @@ def test_BDD_to_MIP_wg(S, f, c, g):
     m, c, v, x = add_BDD_to_MIP(D=C, model=m, x=x, prefix="C_")
     m.display()
 
+
 def generate_test_figures():
     """Generates a simple test instance and creates PDFs."""
 
-
-    S = [[1,2], [1,2,3], [2,3,4], [2,4]]
+    S = [[1, 2], [1, 2, 3], [2, 3, 4], [2, 4]]
     f = {1: 0.1, 2: 0.2, 3: 0.3, 4: 0.4}
-    c = {(1, 1): 0.11,  (1, 2): 0.12,  (2, 1): 0.21,  (2, 2): 0.22, (2, 3): 0.23,
-         (2, 4): 0.24, (3, 2): 0.32,  (3, 3): 0.33,  (3, 4): 0.34,
+    c = {(1, 1): 0.11, (1, 2): 0.12,
+         (2, 1): 0.21, (2, 2): 0.22, (2, 3): 0.23, (2, 4): 0.24,
+         (3, 2): 0.32, (3, 3): 0.33, (3, 4): 0.34,
          (4, 3): 0.43, (4, 4): 0.44}
     g = {
         (1, 0): 0,  (2, 0): 0,  (3, 0): 0,  (4, 0): 0,
@@ -510,7 +489,7 @@ def generate_test_figures():
 
     draw_problem_dia(S, f, c, g)
 
-    C = create_covering_BDD_wg(S, c, g)
+    C = create_covering_BDD_wg(S, g)
     C.show(x_prefix='', filename="tst_covering.dot", dir="run_logs")
     A = create_availability_BDD(S, f)
     A.show(x_prefix='', filename="tst_availability.dot", dir="run_logs")
@@ -519,17 +498,18 @@ def generate_test_figures():
     m, c, v, x = add_BDD_to_MIP(D=C, model=m, x=x, prefix="C_")
     m.display()
 
-if __name__ == '__main__':
+
+def main():
     """Runs when called from a command line"""
     # Procedure that is run if executed from the command line
 
-    S = [[1], [1,2], [1,2], [2]]
-    f = {1: 0.5, 2: 0.7}
-    c = {(1, 1): 0.11,  (1, 2): 0.12,  (2, 2): 0.22,  (1, 3): 0.13,  (2, 3): 0.23,  (2, 4): 0.24}
+    S = [[1], [1, 2], [1, 2, 3], [2, 3]]
+    f = {1: 0.1, 2: 0.2, 3: 0.3}
     g = {
         (1, 0): 0,  (2, 0): 0,  (3, 0): 0,  (4, 0): 0,
         (1, 1): 1,  (2, 1): 1,  (3, 1): 1,  (4, 1): 1,
-        (1, 2): 2,  (2, 2): 2,  (3, 2): 2,  (4, 2): 2}
+        (1, 2): 2,  (2, 2): 2,  (3, 2): 2,  (4, 2): 2,
+        (3, 3): 3}
 
     # m = build_MIP(S, f, c, g)
     # print("*==============================================================*")
@@ -538,6 +518,10 @@ if __name__ == '__main__':
     # m.display()
 
     # test_build_simple_MIP()
-    test_BDD_to_MIP_wg(S, f, c, g)
+    test_BDD_to_MIP_wg(S, f, g)
     # generate_test_figures()
-    ##test_BDD_to_MIP()
+    # test_BDD_to_MIP()
+
+
+if __name__ == '__main__':
+    main()
