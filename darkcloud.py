664a2383cbc1e5317921a564d026754faa40e442
Alexey Bochkarev <a@bochkarev.io>
2022-05-09 20:39:14 +0200

    implemented typed darkcloud algo (simple tests passing)

--- darkcloud.py
+++ darkcloud.py
@@ -10,9 +10,13 @@ import numpy as np
 from dataclasses import dataclass
 import gurobipy as gp
 from experiments.softcover import generate_overlaps, assert_instance
-from BDD import BDD, NTRUE
+from BDD import BDD, NTRUE, NFALSE, NROOT, intersect
 import pytest
 from time import time
+from copy import copy
+import varseq as vs
+import BB_search as bb
+from UFL import create_NF
 
 
 @dataclass
@@ -69,7 +73,7 @@ def gen_caveman_inst(n=10, M=5, L=0.5, verbose=False):
             n_edges += 1
 
         # add edges to ensure the required sparsity
-        while (1- 2*n_edges / (M*(M-1))) > L:
+        while (1 - 2*n_edges / (M*(M-1))) > L:
             n1 = np.random.choice(lastcave)
             n2 = np.random.choice(lastcave)
 
@@ -104,7 +108,8 @@ def gen_typed_cavemen_inst(n, M, L, K, kb_max):
         caves (list of ptscloud), k (point types), kbar (type budgets)
     """
     S, f, c, caves = gen_caveman_inst(n, M, L)
-    join_pts = np.unique(sum([list(c.e1 + c.e2) for c in caves], []))
+    join_pts = sum([list(c.e1 + c.e2) for c in caves], [])
+    join_pts = np.unique([j for j in join_pts if j is not None])
     k = {pt: np.random.randint(1, K+1) for pt in join_pts}  # point types
     kbar = [np.random.randint(1, kb_max+1) for _ in range(K)]
     return S, f, c, caves, k, kbar
@@ -372,9 +377,171 @@ class DDSolver:
         return self.B
 
 
-## Testing code ######################################################
+class DDTypedSolver (DDSolver):
+    def __init__(self, S, f, c, caves, k, kbar):
+        DDSolver.__init__(self, S, f, c, caves)
+        self.k = k
+        self.kbar = kbar
+        self.D = None
 
+    def build_type_DD(self):
+        """Builds a BDD encoding location-level constraints.
+
+        Deals with no. locations per type and location costs.
+
+        Args:
+            f (dict): location costs,
+            types (dict): type codes per facility
+            k_bar (np.array): max. number of locations per type.
+            preferred_order (list): order of nodes to align to, as possible
+                                    (e.g., of the already built cover DD)
+
+        Returns:
+            D (class BDD): resulting diagram.
+            nl (dict): string node labels ("states"), `id` -> `label` (string).
+
+            Implementation based on :py:func:`tUFLP.build_type_DD`.
+        """
+        self.D = BDD(N=len(self.k),
+                     vars=[f"stub{i}" for i in range(1, len(self.k)+1)],
+                     weighted=True)
+        # a *state* is the number of located facilities
+        # for the *current* type (a single number)
+        n = 1  # (original) nodes counter
+        N = len(self.k)
+
+        node_labels = dict({NROOT: 0})
+        next_layer = {0: self.D.addnode(None)}
+        trash_pipe = None
+
+        customers = {c: [] for c in range(1, len(self.k)+1)}
+        for j in self.B.vars:
+            if j in self.k:
+                customers[self.k[j]].append(j)
+
+        types = list(np.random.permutation([(c+1)
+                                            for c in range(len(self.kbar))]))
+
+        for c in types:
+            for customer in customers[c][:-1]:
+                if n == N:
+                    break
+
+                current_layer = copy(next_layer)
+                next_layer = dict()
+                if trash_pipe is not None:
+                    # create a new "false" running node.
+                    new_tp = self.D.addnode(trash_pipe, "lo")
+                    self.D.llink(trash_pipe, new_tp, "hi")
+                    trash_pipe = new_tp
+                    node_labels.update({trash_pipe.id: "💀"})
+
+                for state in current_layer:
+                    node = current_layer[state]
+                    if state in next_layer:
+                        self.D.llink(node, next_layer[state], "lo")
+                    else:
+                        newnode = self.D.addnode(node, "lo")
+                        next_layer.update({state: newnode})
+                        node_labels.update({newnode.id: str(state)})
+
+                    if (state+1) in next_layer:
+                        self.D.llink(node, next_layer[state+1], "hi",
+                                     edge_weight=0.0)
+                    else:
+                        if (state+1) > self.kbar[c-1]:
+                            if trash_pipe is None:
+                                trash_pipe = self.D.addnode(node, "hi")
+                                node_labels.update({trash_pipe.id: "💀"})
+                            else:
+                                self.D.llink(node, trash_pipe, "hi")
+                        else:
+                            newnode = self.D.addnode(node, "hi")
+                            next_layer.update({state+1: newnode})
+                            node_labels.update({newnode.id: str(state+1)})
+                self.D.rename_vars({f"stub{n}": customer})
+                n += 1
+
+            # Processing the last customer separately
+            # (within a type)
+
+            if n < N:
+                current_layer = copy(next_layer)
+                next_layer = dict()
+
+                if trash_pipe is not None:
+                    # create a new "false" running node.
+                    new_tp = self.D.addnode(trash_pipe, "lo")
+                    self.D.llink(trash_pipe, new_tp, "hi")
+                    trash_pipe = new_tp
+                    node_labels.update({trash_pipe.id: "💀"})
+
+                for state in current_layer:
+                    node = current_layer[state]
+                    new_state = 0  # we 'reset' the type counter
+                    if new_state in next_layer:
+                        self.D.llink(node, next_layer[new_state], "lo")
+                    else:
+                        newnode = self.D.addnode(node, "lo")
+                        next_layer.update({new_state: newnode})
+                        node_labels.update({newnode.id: str(new_state)})
+
+                    if (state+1) > self.kbar[c-1]:
+                        if trash_pipe is None:
+                            trash_pipe = self.D.addnode(node, "hi")
+                            node_labels.update({trash_pipe.id: "💀"})
+                        else:
+                            self.D.llink(node, trash_pipe, "hi")
+                    else:
+                        self.D.llink(node, next_layer[new_state], "hi")
+
+                self.D.rename_vars({f"stub{n}": customers[c][-1]})
+                n += 1
+            else:
+                # the last layer of the DD
+                if trash_pipe is not None:
+                    self.D.link(trash_pipe.id, NFALSE, "hi")
+                    self.D.link(trash_pipe.id, NFALSE, "lo")
+
+                for state in next_layer:
+                    node_id = next_layer[state].id
+                    if state+1 > self.kbar[c-1]:
+                        y_target = NFALSE
+                    else:
+                        y_target = NTRUE
+
+                    self.D.link(node_id, y_target, "hi")
+                    self.D.link(node_id, NTRUE, "lo")
+
+                self.D.rename_vars({f"stub{n}": customers[c][-1]})
+        return self.D, node_labels
+
+    def solve_with_DDs(self):
+        """Solves the problem using the DD-based approach."""
+        C = self.build_cover_DD()
+        T, _ = self.build_type_DD()
+        vs_C = vs.VarSeq(C.vars, [len(L) for L in C.layers[:-1]])
+        vs_T = vs.VarSeq(T.vars, [len(L) for L in T.layers[:-1]])
+
+        assert set(vs_C.layer_var) == set(
+            vs_T.layer_var), f"T:{vs_T.layer_var}, C:{vs_C.layer_var}"
+
+        b = bb.BBSearch(vs_C, vs_T)
+
+        # bb.TIMEOUT_ITERATIONS=10000
+        status = b.search()
+        assert status == "optimal" or status == "timeout"
+
+        Cp = C.align_to(b.Ap_cand.layer_var, inplace=False)
+        Tp = T.align_to(b.Ap_cand.layer_var, inplace=False)
+
+        int_DD = intersect(Cp, Tp)
+        sp = int_DD.shortest_path()
+        return sp[0]
+
+# Testing code ######################################################
 # First: test that instances are correct
+
 
 @pytest.mark.parametrize("_", [None for _ in range(30)])
 def test_inst_gen(_):
@@ -399,7 +566,7 @@ def solve_with_MIP(S, f, c):
         c (list): location costs per point.
 
     Returns:
-        objective value, x, and y
+        model, objective, x, y
     """
     m = gp.Model()
     m.modelSense = gp.GRB.MINIMIZE
@@ -469,9 +636,9 @@ def solve_typed_with_MIP(S, f, c, k, kbar):
             m.addConstr(y[(j, a)] >= y[(j, a+1)])
 
     # add type constraints
-    for t in range(len(kbar)):
+    for t in range(1, len(kbar)+1):
         m.addConstr(gp.quicksum(x[j] for j in k
-                                if k[j] == t) <= kbar[t])
+                                if k[j] == t) <= kbar[t-1])
 
     m.update()
     m.optimize()
@@ -486,7 +653,7 @@ def test_BDD_vs_MIP_simple(_):
     assert compare_BDD_vs_MIP(S, f, c, caves)
 
 
-@pytest.mark.parametrize("_", [None for _ in range(100)])
+@pytest.mark.parametrize("_", [None for _ in range(10)])
 def test_BDD_vs_MIP_random(_):
     n = np.random.randint(5, 10)
     M = np.random.randint(3, 10)
@@ -495,7 +662,7 @@ def test_BDD_vs_MIP_random(_):
     assert compare_BDD_vs_MIP(S, f, c, caves)
 
 
-def compare_BDD_vs_MIP(S,f,c,caves):
+def compare_BDD_vs_MIP(S, f, c, caves):
     sol = DDSolver(S, f, c, caves)
     B = sol.build_cover_DD()
     sp = B.shortest_path()
@@ -504,6 +671,28 @@ def compare_BDD_vs_MIP(S,f,c,caves):
     print(f"obj={obj}, sp={sp[0]}")
     return abs(obj - sp[0]) < 1e-3
 
+
+@pytest.mark.parametrize("_", [None for _ in range(10)])
+def test_DD_full(_):
+    """Tests the DD-based approach against a MiP."""
+    print("Generating instance and solving a MiP (twice)...")
+    generated = False
+    while not generated:
+        S, f, c, caves, k, kbar = gen_typed_cavemen_inst(10, 10, 0.25, 3, 2)
+        t0 = time()
+        _, objMIP, _, _ = solve_typed_with_MIP(S, f, c, k, kbar)
+        t1 = time()
+        _, objUC, _, _ = solve_with_MIP(S, f, c)  # for UnConstrained
+        if abs(objMIP - objUC) > 1:
+            generated = True
+
+    print(f"Done, solved with MIP in {t1 - t0:.1f} sec. Solving with DDs...")
+    t0 = time()
+    sol = DDTypedSolver(S, f, c, caves, k, kbar)
+    objDD = sol.solve_with_DDs()
+    print(f"Done after {time()-t0:.1f} sec.")
+    print(f"MIP: {objMIP:.2f},\nDDs: {objDD:.2f}")
+    assert abs(objMIP - objDD) < 0.01
 
 def main():
     """Main experiment: runtimes DD vs MIP."""
@@ -529,7 +718,7 @@ def main():
 
         print(f"{k}, {n*M}, {n}, {M}, {L:.2f}, {t_gen:.3f}, {t_BDD:.3f}, {t_MIP:.3f}, {t_MIP/t_BDD:.1f}",
               flush=True)
-        assert abs(obj_MIP - obj_BDD)<1e-2
+        assert abs(obj_MIP - obj_BDD) < 1e-2
 
 
 if __name__ == '__main__':
