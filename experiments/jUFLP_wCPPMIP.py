39ea58dc1e268be5270ced58439e54f29c2fbf33
Alexey Bochkarev <a@bochkarev.io>
2022-06-02 23:30:32 +0200

    outlined the note on j-UFLP

--- experiments/jUFLP_wCPPMIP.py
+++ experiments/jUFLP_wCPPMIP.py
@@ -1,20 +1,24 @@
-"""An experiment for joint UFLP + special instance type (cavemen)."""
+"""An experiment for joint UFLP + special instance type (cavemen).
+
+A version with CPP MIP.
+"""
 from jUFLP_cavemen import gen_cavemen_jUFLP_inst, solve_cm_jUFLP_MIP
-from jUFLP_cavemen import solve_cm_jUFLP_DDs, save_inst
+from jUFLP_cavemen import solve_cm_jUFLP_CPPMIP, solve_cm_jUFLP_DDs
+from jUFLP_cavemen import save_inst
 from UFL import add_BDD_to_MIP
 from time import time
 import gurobipy as gp
 
 
 def main():
-    print("experiment, n, M, L, tMIP, tDD_toA, tDD_toB, tDD_VS, int_toA, int_toB, int_VS")
+    print("experiment, n, M, L, tMIP, tMIP_CPP, tDD_toA, tDD_toB, tDD_VS, int_toA, int_toB, int_VS")
     M = 5
     L = 0.25
 
     i = 1
 
     while True:
-        for n in range(15, 18):
+        for n in range(5, 16):
             i1, i2, jm = gen_cavemen_jUFLP_inst(n, M, L)
             save_inst(i1, i2, jm, f"instances/jUFLP_cm/inst_wCPPMIP_{i}.json")
 
@@ -24,9 +28,8 @@ def main():
 
             # solve with CPP MIP
             t0 = time()
-            tMIP_CPP = time()
-
-            assert m.status == gp.GRB.OPTIMAL
+            objMIP_CPP = solve_cm_jUFLP_CPPMIP(i1, i2, jm)
+            tMIP_CPP = time() - t0
 
             t0 = time()
             objDD, int_toA = solve_cm_jUFLP_DDs(i1, i2, jm, "toA", True)
@@ -43,6 +46,7 @@ def main():
             assert abs(objMIP - objDD)< 0.01
             assert abs(objMIP - objDD2)< 0.01
             assert abs(objMIP - objDD3)< 0.01
+            assert abs(objMIP - objMIP_CPP) < 0.01
 
             print(f"{i}, {n}, {M}, {L}, {tMIP:.2f}, {tMIP_CPP:.2f}, {tDD_toA:.2f}, {tDD_toB:.2f}, {tDD_VS:.2f}, {int_toA}, {int_toB}, {int_VS}", flush=True)
 
