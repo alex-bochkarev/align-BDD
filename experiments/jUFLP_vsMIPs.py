1007b171baa10016e41074d9f006d99d4b62251a
Alexey Bochkarev <a@bochkarev.io>
2022-06-20 16:23:29 +0200

    implemented an experiment vs MiPs (special case instances needed)

--- experiments/jUFLP_vsMIPs.py
+++ experiments/jUFLP_vsMIPs.py
@@ -9,7 +9,7 @@ from time import time
 
 
 def main():
-    print("experiment, n, M, L, tMIP, tMIP_CPP, tDD_toA, tDD_toB, tDD_VS, int_toA, int_toB, int_VS")
+    print("experiment, n, M, L, tMIP, tMIP_CPP, tDD_VS, int_VS")
     M = 5
     L = 0.50
 
@@ -33,27 +33,15 @@ def main():
             print(f"✅ CPP MIP in {tMIP_CPP:.2f} sec")
 
             t0 = time()
-            objDD, int_toA = solve_cm_jUFLP_fullDDs(i1, i2, jm, "toA", True)
-            tDD_toA = time() - t0
-            print(f"✅ Full DDs toA in {tDD_toA:.2f} sec")
-
-            t0 = time()
-            objDD2, int_toB = solve_cm_jUFLP_fullDDs(i1, i2, jm, "toB", True)
-            tDD_toB = time() - t0
-
-            print(f"✅ Full DDs toB in {tDD_toB:.2f} sec")
-            t0 = time()
             objDD3, int_VS = solve_cm_jUFLP_fullDDs(i1, i2, jm, "VS", True)
             tDD_VS = time() - t0
             print(f"✅ Full DDs VS in {tDD_VS:.2f} sec")
 
             assert abs(objMIP - objMIP_CPP) < 0.01, f"objMIP = {objMIP:.2f}, objMIP_CPP={objMIP_CPP:.2f}"
-            assert abs(objMIP - objDD)< 0.01, f"objMIP = {objMIP:.2f}, objDD={objDD:.2f}"
-            assert abs(objMIP - objDD2)< 0.01, f"objMIP = {objMIP:.2f}, objDD2={objDD2:.2f}"
             assert abs(objMIP - objDD3)< 0.01, f"objMIP = {objMIP:.2f}, objDD3={objDD3:.2f}"
 
 
-            print(f"{i}, {n}, {M}, {L}, {tMIP:.2f}, {tMIP_CPP:.2f}, {tDD_toA:.2f}, {tDD_toB:.2f}, {tDD_VS:.2f}, {int_toA}, {int_toB}, {int_VS}", flush=True)
+            print(f"{i}, {n}, {M}, {L}, {tMIP:.2f}, {tMIP_CPP:.2f}, {tDD_VS:.2f}, {int_VS}", flush=True)
 
             i += 1
 
